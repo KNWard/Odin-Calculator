bdfdf045722c927bc65f36cf23e61f4bd5fcca80
Kendra <92537842+KNWard@users.noreply.github.com>
2025-05-21 16:54:44 -0400

    Added script tag to HTML file. Added basic math functions to JS file, tested in console.

---
+++ script.js
@@ -0,0 +1,31 @@
+// ADDITION
+function add(num1, num2) {
+ return num1 + num2;
+};
+
+let addResult = add(2, 5);
+console.log(addResult);
+
+// SUBTRACTION
+function subtract(num1, num2) {
+ return num1 - num2;
+};
+
+let subResult = subtract(10, 6);
+console.log(subResult);
+
+// MULTIPLICATION
+function multi(num1, num2) {
+ return num1 * num2;
+};
+
+let multiResult = multi(4, 5);
+console.log(multiResult);
+
+// DIVISION
+function divi(num1, num2) {
+ return num1 / num2;
+};
+
+let diviResult = divi(50, 5);
+console.log(diviResult);
