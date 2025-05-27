786676efcbf0cd2f441ff71856378d33102d9ba2
Kendra <92537842+KNWard@users.noreply.github.com>
2025-05-27 00:18:36 -0400

    Added functions and event listeners to JS for calculator functionality. Removed class name from two buttons in HTML.

--- script.js
+++ script.js
@@ -1,51 +1,145 @@
-const buttons = document.querySelectorAll('.btn');
-const display = document.querySelector('textarea');
-const clearBtn = document.querySelector('.clear');
-let characters = [];
+// VARIABLES
+let operator = '';
+let firstNum = '';
+let secondNum = '';
+let isSecondNum = false;
 
-buttons.forEach(btn => {
- btn.addEventListener('click', () => {
-  display.value += btn.innerText
-  characters = display.value.split('')
+
+document.addEventListener("DOMContentLoaded", () => {
+ let numbers = document.querySelectorAll(".number");
+ let operators = document.querySelectorAll(".operator");
+ let clear = document.querySelector(".clear");
+ let decimal = document.querySelector(".decimal");
+ let equal = document.querySelector(".equal");
+ let display = document.querySelector("#display");
+
+ numbers.forEach((number) => number.addEventListener("click", (e) => {
+  handleNumber(e.target.textContent)
+  display.textContent = isSecondNum ? secondNum :  firstNum;
+ }))
+
+ operators.forEach((ops) => ops.addEventListener("click", (e) => {
+  handleOperator(e.target.textContent)
+  display.textContent = firstNum + "" + operator;
+ }))
+
+ clear.addEventListener("click", () => {
+  firstNum = '';
+  secondNum = '';
+  operator = '';
+  isSecondNum = false;
+  display.textContent = '';
+ })
+
+ equal.addEventListener("click", () => {
+  calc();
+  display.textContent = firstNum;
  })
 })
 
-clearBtn.addEventListener('click', () => {
- display.value = "";
-})
+function handleNumber(num) {
+ if (!isSecondNum) {
+    firstNum += num;
+  } else {
+    secondNum += num;
+  }
+}
+
+function handleOperator(ops) {
+ if (firstNum !== '') {
+  operator = ops;
+  isSecondNum = true;
+ }
+}
+
+function calc() {
+ const num1 = Number(firstNum);
+ const num2 = Number(secondNum);
+
+ if (isNaN(num1) || isNaN(num2)) {
+  console.log("Invalid input");
+  return;
+ }
+
+ if (operator === "+") {
+  firstNum = num1 + num2;
+ } else if (operator === "-") {
+  firstNum = num1 - num2;
+ } else if (operator === "x") {
+  firstNum = num1 * num2;
+ } else if (operator === "/") {
+  firstNum = num2 !== 0 ? num1 / num2 : "Error";
+ }
+
+ // RESET FOR NEXT CALCULATION
+ secondNum = '';
+ operator = '';
+ isSecondNum = false;
+
+ console.log(firstNum);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const buttons = document.querySelectorAll('.btn');
+// const display = document.querySelector('textarea');
+// const clearBtn = document.querySelector('.clear');
+// let characters = [];
+
+// buttons.forEach(btn => {
+//  btn.addEventListener('click', () => {
+//   display.value += btn.innerText
+//   characters = display.value.split('')
+//  })
+// })
+
+
+// clearBtn.addEventListener('click', () => {
+//  display.value = "";
+// })
 
 
 // VARIABLES
 
 
 // ADDITION
-function add(num1, num2) {
- return num1 + num2;
-};
+// function add(num1, num2) {
+//  return num1 + num2;
+// };
 
-let addResult = add(2, 5);
-console.log(addResult);
+// let addResult = add(2, 5);
+// console.log(addResult);
 
 // SUBTRACTION
-function subtract(num1, num2) {
- return num1 - num2;
-};
+// function subtract(num1, num2) {
+//  return num1 - num2;
+// };
 
-let subResult = subtract(10, 6);
-console.log(subResult);
+// let subResult = subtract(10, 6);
+// console.log(subResult);
 
 // MULTIPLICATION
-function multi(num1, num2) {
- return num1 * num2;
-};
+// function multi(num1, num2) {
+//  return num1 * num2;
+// };
 
-let multiResult = multi(4, 5);
-console.log(multiResult);
+// let multiResult = multi(4, 5);
+// console.log(multiResult);
 
 // DIVISION
-function divi(num1, num2) {
- return num1 / num2;
-};
+// function divi(num1, num2) {
+//  return num1 / num2;
+// };
 
-let diviResult = divi(50, 5);
-console.log(diviResult);
+// let diviResult = divi(50, 5);
+// console.log(diviResult);
